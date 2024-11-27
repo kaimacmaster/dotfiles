48335d995eb342e767e38a4f2eb47601046b02d1
Kai Macmaster <kai@macmaster.co.uk>
2024-11-27 03:35:17 +0000

    lsp goodness

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,170 +1,156 @@
+-- ==========================
+-- Neovim Lua Configuration
+-- ==========================
+
+-- ====== General Settings ======
 local vim = vim
-local Plug = vim.fn['plug#']
 
--- Set keybinds
-vim.g.mapleader = ',';
-vim.g.maplocalleader = ',';
+-- Set leader keys
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
--- System clipboard
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>p', '"+p')
-vim.keymap.set('v', '<leader>y', '"+y')
-vim.keymap.set('v', '<leader>p', '"+p')
+-- Basic Options
+vim.opt.tabstop = 2               -- Set tab width for display
+vim.opt.shiftwidth = 2            -- Number of spaces for (auto)indent
+vim.opt.expandtab = true          -- Convert tabs to spaces
+vim.bo.softtabstop = 2            -- Number of spaces a Tab press inserts
+vim.opt.scrolloff = 10            -- Always keep 10 lines in view
+vim.opt.ignorecase = true         -- Ignore case in searches
+vim.opt.smartcase = true          -- Override ignorecase if uppercase is used
+vim.opt.completeopt = 'menuone,noinsert,noselect' -- Completion options
+vim.opt.number = true             -- Show line numbers
 
--- Move lines up and down with Alt/Opt
-vim.keymap.set('n', '<A-Down>', ':m+1<CR>==', { noremap = true, silent = true })
-vim.keymap.set('n', '<A-Up>', ':m-2<CR>==', { noremap = true, silent = true })
-vim.keymap.set('v', '<A-Down>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.keymap.set('v', '<A-Up>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-
-vim.keymap.set('n', '<A-j>', ':m+1<CR>==', { noremap = true, silent = true })
-vim.keymap.set('n', '<A-k>', ':m-2<CR>==', { noremap = true, silent = true })
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-
--- Force quit without saving
-vim.keymap.set('n', '<leader>q', ':q!<CR>', { noremap = true, silent = true })
-
--- Remove quotes from keys in an object
-vim.keymap.set('n', '<leader>dq', [[:%s/['"]\(\w\+\)['"]:/\1:/g<CR>]], { noremap = true, silent = true })
-
--- Set tab width for the visual appearance of tab characters
-vim.opt.tabstop = 2
-
--- Set the number of spaces to use for each step of (auto)indent
-vim.opt.shiftwidth = 2
-
--- Convert tabs to spaces
-vim.opt.expandtab = true
-
--- Set the number of spaces a Tab key press inserts
-vim.bo.softtabstop = 2
-
--- Ensure there are always 10 lines in view
-vim.opt.scrolloff = 10
-
--- Ignore case when searching, unless an uppercase letter is used
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Disable esc from exiting insert mode
+-- Disable Esc from exiting insert mode
 vim.api.nvim_set_keymap('i', '<Esc>', '<Nop>', { noremap = true, silent = true })
+
+-- ====== Plugin Management ======
+
+-- Initialize vim-plug
+local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
+-- Git and Git Signs
 Plug('tpope/vim-fugitive')
+Plug('lewis6991/gitsigns.nvim')
+
+-- UI Enhancements
 Plug('folke/noice.nvim')
 Plug('MunifTanjim/nui.nvim')
 Plug('rcarriga/nvim-notify')
-Plug('lewis6991/gitsigns.nvim')
 Plug('tpope/vim-surround')
 Plug('tpope/vim-repeat')
 Plug('tpope/vim-abolish')
 Plug('preservim/nerdcommenter')
 Plug('loctvl842/monokai-pro.nvim')
 Plug('itchyny/lightline.vim')
+
+-- Fuzzy Finder
 Plug('nvim-telescope/telescope.nvim', { tag = '0.1.8' })
 Plug('nvim-lua/plenary.nvim')
+
+-- Treesitter for Syntax Highlighting
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' }) 
+
+-- Tmux Integration
 Plug('edkolev/tmuxline.vim')
+
+-- LSP and Autocompletion
 Plug('neovim/nvim-lspconfig')
 Plug('ms-jpq/coq_nvim', { branch = 'coq' })
 Plug('ms-jpq/coq.artifacts', { branch = 'artifacts' })
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('onsails/lspkind-nvim')
+Plug('L3MON4D3/LuaSnip')
+
+-- File Explorer
 Plug('ms-jpq/chadtree')
 Plug('nvim-lua/popup.nvim')
+
+-- Mason for LSP Management
 Plug('williamboman/mason.nvim')
 Plug('williamboman/mason-lspconfig.nvim')
+
+-- AI Assistance
 Plug('github/copilot.vim')
+
+-- LSP Enhancements
+Plug('nvimdev/lspsaga.nvim')
+
+-- Code Analysis
 Plug('dense-analysis/ale')
+
+-- Lightbulb Indicator
 Plug('kosayoda/nvim-lightbulb')
 
 vim.call('plug#end')
 
--- Noice
-require("noice").setup({
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-    },
-  },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
+-- ====== Plugin Configurations ======
+
+-- ====== GitSigns ======
+require('gitsigns').setup({
+  current_line_blame = true,
+  current_line_blame_opts = {
+    delay = 300,
   },
 })
 
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+-- ====== LSPSaga ======
+require('lspsaga').setup({
+  folder_level = 2,
+  symbol_in_winbar = {
+    file_formatter = function(opts, _, _)
+      -- Get the file icon and highlight group
+      local file_icon, hl_group = require('lspsaga.symbolwinbar').get_file_icon()
+      -- Return the formatted string with a space after the icon
+      return file_icon .. ' ' .. opts.filename
+    end
+  },
+})
 
--- Set theme
+-- ====== Noice ======
+require("noice").setup({
+  lsp = {
+    -- Override markdown rendering to use Treesitter
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- Requires hrsh7th/nvim-cmp
+    },
+  },
+  presets = {
+    bottom_search = true,      -- Classic bottom cmdline for search
+    command_palette = true,    -- Position cmdline and popupmenu together
+    long_message_to_split = true, -- Send long messages to a split
+    inc_rename = false,        -- Disable input dialog for inc-rename.nvim
+    lsp_doc_border = false,    -- No border for hover docs and signature help
+  },
+})
+
+-- ====== Telescope ======
+require("telescope").setup({})
+-- Already have keybindings set above
+
+-- ====== Theme ======
 require("monokai-pro").setup({
   transparent_background = true,
   terminal_colors = false,
   filter = "spectrum",
 })
-
 vim.cmd([[colorscheme monokai-pro]])
-vim.cmd([[set number]])
+
+-- Lightline Configuration
 vim.cmd([[let g:lightline = {'colorscheme': 'monokaipro'}]])
 
-require("telescope")
+-- ====== Mason ======
 require('mason').setup()
 require('mason-lspconfig').setup({
   ensure_installed = { 'vuels', 'volar', 'eslint', 'html', 'tailwindcss', 'cssls' },
 })
 
--- Setup LSP config for Volar
-local lspconfig = require('lspconfig')
-local coq = require('coq')
-
--- Use an on_attach function to only map the following keys
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  local opts = { noremap=true, silent=true }
-end
-
-lspconfig.vuels.setup(coq.lsp_ensure_capabilities({
-  on_attach = on_attach,
-  filetypes = { 'vue' },
-}))
-
---lspconfig.volar.setup(coq.lsp_ensure_capabilities({
---  filetypes = { '' },
---}))
-
-lspconfig.ts_ls.setup(coq.lsp_ensure_capabilities({
-  on_attach = on_attach,
-  filetypes = { 'typescript', 'javascript','vue' },
-}))
-
-lspconfig.tailwindcss.setup(coq.lsp_ensure_capabilities({
-  on_attach = on_attach,
-  filetypes = { 'vue' },
-}))
-
-lspconfig.cssls.setup(coq.lsp_ensure_capabilities({
-  on_attach = on_attach,
-  filetypes = { 'vue', 'css', 'scss', 'less' },
-}))
-
-lspconfig.html.setup(coq.lsp_ensure_capabilities({
-  on_attach = on_attach,
-  filetypes = { 'html' },
-}))
-
+-- ====== Treesitter ======
 require("nvim-treesitter.configs").setup({
   ensure_installed = { 'vue', 'typescript', 'javascript', 'css', 'scss', 'html' },
   highlight = {
@@ -172,12 +158,14 @@ require("nvim-treesitter.configs").setup({
   }
 })
 
+-- ====== Nvim Lightbulb ======
 require("nvim-lightbulb").setup({
   priority = 1000,
   autocmd = { enabled = true },
-  sign = { text = "C"}
-});
+  sign = { text = " " }, -- Changed to a more visible icon
+})
 
+-- ====== CHADTree ======
 vim.api.nvim_set_var('chadtree_settings', {
     options = {
         follow = true,
@@ -192,13 +180,10 @@ vim.api.nvim_set_var('chadtree_settings', {
         h_split = {'h'},
         v_split = {'v'},
     }
-});
+})
+-- Open CHADTree with <leader>o already set above
 
---vim.cmd([[CHADopen]])
---open chattree with <leader>o
-vim.keymap.set('n', '<leader>o', ':CHADopen<CR>', { noremap = true, silent = true })
-
--- ALE
+-- ====== ALE (Asynchronous Lint Engine) ======
 vim.g.ale_enabled = 1
 vim.g.ale_linters_explicit = 1
 vim.g.ale_fixers = {
@@ -211,15 +196,184 @@ vim.g.ale_fixers = {
   html = { 'prettier' },
 }
 
-vim.keymap.set('n', '<leader>lp', ':ALEFix<CR>', { noremap = true, silent = true })
-vim.keymap.set('v', '<leader>lp', ':ALEFix<CR>', { noremap = true, silent = true })
+-- ====== LSP Configuration ======
+local Path = require('plenary.path')
+local lspconfig = require('lspconfig')
+local coq = require('coq')
+-- ====== LSP Configuration ======
 
-vim.keymap.set('n', '<leader>sv', ':vnew<CR>' , { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>sh', ':split<CR>' , { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>st', ':tabnew<CR>' , { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>so', ':only<CR>' , { noremap = true, silent = true })
+-- Initialize the list of LSP servers
+local servers = { 'ts_ls', 'tailwindcss', 'cssls', 'html', 'vuels', 'volar' }
 
--- Auto close empty tags ></div> or ></span> etc by replacing with /> and moving cursor inside the tag
---vim.keymap.set('n', '<leader>ac', ':%s/<\/\(\w\+\)>/<\1\/>/g<CR>:%s/<\(\w\+\)\/>/<<\1\/>><CR>:%s/<\(\w\+\)\/>><CR>')
---vim.keymap.set('n', '<leader>ac', ':%s { noremap = true, silent = true })
+-- Define a helper function for consistent keybindings within LSP's on_attach
+local function on_attach(client, bufnr)
+  local opts = { noremap=true, silent=true }
+  local keymap = vim.api.nvim_buf_set_keymap
+
+  -- Define LSP-related keybindings
+  keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+end
+
+-- Setup each LSP server
+for _, server in ipairs(servers) do
+  -- Define filetypes consistently as arrays
+  local ft = {}
+  if server == 'volar' or server == 'vuels' then
+    ft = { 'vue' }
+  elseif server == 'tsserver' then
+    ft = { 'typescript', 'javascript', 'vue' }
+  elseif server == 'tailwindcss' then
+    ft = { 'vue' }
+  elseif server == 'cssls' then
+    ft = { 'vue', 'css', 'scss', 'less' }
+  elseif server == 'html' then
+    ft = { 'html' }
+  end
+
+  -- Setup the LSP server with COQ and custom on_attach
+  lspconfig[server].setup(coq.lsp_ensure_capabilities({
+    on_attach = on_attach,
+    filetypes = ft,
+  }))
+end
+
+-- ====== Nvim-Notify ======
+require('notify').setup({
+  stages = 'fade',
+  timeout = 5000,
+  background_colour = '#1e222a',
+  text_colour = '#abb2bf',
+  icons = {
+    ERROR = ' ',
+    WARN = ' ',
+    INFO = ' ',
+    DEBUG = ' ',
+    TRACE = '✎ ',
+  },
+})
+
+-- ====== nvim-cmp (Autocompletion) ======
+
+-- Protected call to ensure 'cmp' is available
+local status_cmp, cmp = pcall(require, 'cmp')
+if not status_cmp then
+  return
+end
+
+-- Protected call to ensure 'lspkind' is available
+local status_lspkind, lspkind = pcall(require, 'lspkind')
+if not status_lspkind then
+  return
+end
+
+-- Protected call to ensure 'luasnip' is available
+local status_luasnip, luasnip = pcall(require, 'luasnip')
+if not status_luasnip then
+  return
+end
+
+-- Load friendly snippets (optional, if you have any)
+require("luasnip.loaders.from_vscode").lazy_load()
+
+-- nvim-cmp setup
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body) -- Use LuaSnip for snippet expansion
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4), -- Scroll docs down
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),  -- Scroll docs up
+    ['<C-Space>'] = cmp.mapping.complete(),   -- Trigger completion
+    ['<C-e>'] = cmp.mapping.close(),          -- Close completion menu
+    ['<CR>'] = cmp.mapping.confirm({         -- Confirm selection
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' }, -- LSP completions
+    { name = 'luasnip' },  -- Snippets
+    { name = 'buffer' },   -- Buffer completions
+  }),
+  formatting = {
+    format = lspkind.cmp_format({ -- Use lspkind for formatting
+      with_text = false,
+      maxwidth = 50,
+      ellipsis_char = '...',
+    }),
+  },
+})
+
+-- ====== Keybindings ======
+
+-- Helper function for setting keymaps
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Clipboard Operations
+keymap('n', '<leader>y', '"+y', opts)
+keymap('n', '<leader>p', '"+p', opts)
+keymap('v', '<leader>y', '"+y', opts)
+keymap('v', '<leader>p', '"+p', opts)
+
+-- Move Lines Up and Down with Alt/Opt
+local move_opts = { noremap = true, silent = true }
+keymap('n', '<A-Down>', ':m+1<CR>==', move_opts)
+keymap('n', '<A-Up>', ':m-2<CR>==', move_opts)
+keymap('v', '<A-Down>', ":m '>+1<CR>gv=gv", move_opts)
+keymap('v', '<A-Up>', ":m '<-2<CR>gv=gv", move_opts)
+
+keymap('n', '<A-j>', ':m+1<CR>==', move_opts)
+keymap('n', '<A-k>', ':m-2<CR>==', move_opts)
+keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", move_opts)
+keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", move_opts)
+
+-- Force Quit Without Saving
+keymap('n', '<leader>q', ':q!<CR>', opts)
+
+-- Remove Quotes from Keys in an Object
+keymap('n', '<leader>dq', [[:%s/['"]\(\w\+\)['"]:/\1:/g<CR>]], opts)
+
+-- Open CHADTree
+keymap('n', '<leader>o', ':CHADopen<CR>', opts)
+
+-- Telescope Keybindings
+local builtin = require('telescope.builtin')
+keymap('n', '<leader>ff', builtin.find_files, opts)
+keymap('n', '<leader>fg', builtin.live_grep, opts)
+
+-- ALE Fix Keybindings
+keymap('n', '<leader>lp', ':ALEFix<CR>', opts)
+keymap('v', '<leader>lp', ':ALEFix<CR>', opts)
+
+-- Split and Tab Management
+keymap('n', '<leader>sv', ':vnew<CR>', opts)
+keymap('n', '<leader>sh', ':split<CR>', opts)
+keymap('n', '<leader>st', ':tabnew<CR>', opts)
+keymap('n', '<leader>so', ':only<CR>', opts)
+
+-- Lspsaga keybindings
+keymap('n', 'ca', ':Lspsaga code_action<CR>', opts)
+--vim.keymap.set({'n','t', '<A-d>', '<cmd>Lspsaga term_toggle'})
+keymap('n', 'gt', ':Lspsaga term_toggle<CR>', opts)
+
+-- ==========================
+-- End of Configuration
+-- ==========================
 
