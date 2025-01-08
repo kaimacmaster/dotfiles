d237f30d5f506ac4ac7a6ef50bd6548adc4b8579
Kai Macmaster <kai@macmaster.co.uk>
2025-01-08 15:11:31 +0000

    Main to Lazy

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -41,3 +41,57 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+-- ====== Keybindings ======
+
+-- Helper function for setting keymaps
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Clipboard Operations
+keymap('n', '<leader>y', '"+y', opts)
+keymap('n', '<leader>p', '"+p', opts)
+keymap('v', '<leader>y', '"+y', opts)
+keymap('v', '<leader>p', '"+p', opts)
+
+-- Move Lines Up and Down with Alt/Opt
+local move_opts = { noremap = true, silent = true }
+keymap('n', '<A-Down>', ':m+1<CR>==', move_opts)
+keymap('n', '<A-Up>', ':m-2<CR>==', move_opts)
+keymap('v', '<A-Down>', ":m '>+1<CR>gv=gv", move_opts)
+keymap('v', '<A-Up>', ":m '<-2<CR>gv=gv", move_opts)
+
+keymap('n', '<A-j>', ':m+1<CR>==', move_opts)
+keymap('n', '<A-k>', ':m-2<CR>==', move_opts)
+keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", move_opts)
+keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", move_opts)
+
+-- Force Quit Without Saving
+keymap('n', '<leader>q', ':q!<CR>', opts)
+
+-- Remove Quotes from Keys in an Object
+keymap('n', '<leader>dq', [[:%s/['"]\(\w\+\)['"]:/\1:/g<CR>]], opts)
+
+-- Telescope Keybindings
+keymap('n', '<leader>ff', ':Telescope find_files<CR>', opts)
+keymap('n', '<leader>fg', ':Telescope live_grep<CR>', opts)
+keymap('n', '<leader>fb', ':Telescope buffers<CR>', opts)
+keymap('n', '<leader>fh', ':Telescope help_tags<CR>', opts)
+
+-- Split and Tab Management
+keymap('n', '<leader>sv', ':vnew<CR>', opts)
+keymap('n', '<leader>sh', ':split<CR>', opts)
+keymap('n', '<leader>st', ':tabnew<CR>', opts)
+keymap('n', '<leader>so', ':only<CR>', opts)
+
+-- Lspsaga keybindings
+keymap('n', 'ca', ':Lspsaga code_action<CR>', opts)
+keymap('n', 'gt', ':Lspsaga term_toggle<CR>', opts)
+
+-- Cheeky save
+keymap('n', '<C-s>', ':w<CR>', opts)
+
+-- File Explorer
+vim.keymap.set("n", "<leader>o", ":NvimTreeToggle path=%:p:h <CR>")
+
+
