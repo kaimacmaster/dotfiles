cb9afdcc09c749f896e5966fbcd1db16208881d8
Kai Macmaster <kai@macmaster.co.uk>
2025-02-11 22:26:21 +0000

    fresh start

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,1 +1,27 @@
-require("config.lazy")
+require 'core.options' -- Load general options
+require 'core.keymaps' -- Load general keymaps
+
+-- Set up the Lazy plugin manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load plugins
+-- Set up plugins
+require('lazy').setup {
+  require 'plugins.neotree',
+  require 'plugins.colortheme',
+  require 'plugins.telescope',
+  require 'plugins.autocomplete',
+  require 'plugins.lsp',
+  require 'plugins.none-ls',
+  require 'plugins.treesitter',
+  require 'plugins.misc',
+  require 'plugins.lualine',
+}
