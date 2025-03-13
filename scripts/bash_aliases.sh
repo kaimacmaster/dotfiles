8e4f328f2ee6c63fb399d256bbc597247926c989
Kai Macmaster <kai@macmaster.co.uk>
2025-03-13 09:23:51 +0000

    Autosave: 2025-03-13 09:23:51

---
+++ scripts/bash_aliases.sh
@@ -0,0 +1,16 @@
+# User configuration
+export EDITOR='nvim'
+
+# Aliases
+alias ll='ls -alF'
+alias yd='nvm use && yarn dev'
+alias yid='nvm use && yarn && yarn dev'
+alias nv='nvim'
+alias gs='git status'
+alias gc='git commit -a -m'
+alias ga='git add'
+alias gp='git push'
+alias gd='git diff'
+alias gl='git log --left-right --decorate --graph'
+alias zshrc='nvim ~/.zshrc'
+
